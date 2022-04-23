ccb116efac75ae8c171569ee735e92d9e8ebdaf2
uncia <puritytune@gmail.com>
2022-04-23 19:32:45 +0900

    포스트 CRUD 작업

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,51 @@
+class PostsController < ApplicationController
+  def index
+    @post = Post.all
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+
+    if @post.save
+        redirect_to @post
+    else
+        render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+
+    if @post.update(post_params)
+        redirect_to @post
+    else
+        render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    
+    redirect_to root_path, status: :see_other
+  end
+
+
+  private
+    def post_params
+        params.require(:post).permit(:title, :content)
+    end
+
+end
---
+++ app/models/post.rb
@@ -0,0 +1,5 @@
+class Post < ApplicationRecord
+    
+  validates :title, presence: true
+  validates :content, presence: true, length: { minimum: 10 }
+end
