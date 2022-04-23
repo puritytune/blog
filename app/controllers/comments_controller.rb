8e75d1c6122f8fe46a33395f2e6cd897ee85d895
uncia <puritytune@gmail.com>
2022-04-23 21:53:14 +0900

    bootstrap 적용

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -6,7 +6,7 @@ class CommentsController < ApplicationController
   end
 
   def destroy
-    @post = Post.find(params[:id])
+    @post = Post.find(params[:post_id])
     @comment = @post.comments.find(params[:id])
     @comment.destroy
 
